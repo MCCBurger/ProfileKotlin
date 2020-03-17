453e91157ee4692e7570e8b59ddf5f58d6248fba
Brian <bburger@students.mchenry.edu>
2020-03-17 11:02:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/profilekotlin/Main2Activity.kt
@@ -0,0 +1,15 @@
+package com.example.profilekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+    }
+}
